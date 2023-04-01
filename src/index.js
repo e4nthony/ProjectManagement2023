a0aeb759d7817e2022d9eaafe530b87e0598f5fb
neo050 <neoray.asking666@gmail.com>
2023-04-01 12:02:38 +0300

    public html test file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 const port = process.env.port || 3000;
 const app = express();
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     console.log('A new request has arrived to index.js');
     res.send('Hello from the server main page');
