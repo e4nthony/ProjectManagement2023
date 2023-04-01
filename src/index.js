cfda03ce0e5089a83bd00906b2cc1a3eeb53319d
Anthony E <e4nthony@gmail.com>
2023-04-02 00:51:24 +0300

    move router to different file (type:module)

--- src/index.js
+++ src/index.js
@@ -1,48 +1,40 @@
 import express from 'express';
 const app = express();
 
-
-
+/** For request/response functionality in func. (middleware) */
 import bodyParser from 'body-parser';
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// const port = process.env.port || 4000;
-const port = 4001;
+const port = 4001;  // const port = process.env.port || 4000;
+
+/** --- Routes --- */
+import default_route from './routes/default_route.js';
+/** 
+ * Default route handler.
+ * ( Mount the routesHandler as middleware at path '/' ).
+ */ 
+app.use('/', default_route);
+
+/** Make files in folder "public" accessible via url. Example: '/public/index.html' . */
+app.use('/public', express.static('public'));
+
+
 
 app.listen(port, () => {
-    console.log('Server is up and runnig at port : ' + port);
+    console.log('Server is up and runnig at : http://localhost:' + port); // TAG: debug
 });
 
 
 
 
 
-// router
-// const express = require('express');
-const routesHandler = express.Router();
-app.use('/', routesHandler); // Mount the routesHandler as middleware at path '/'
-
-routesHandler.get('/hey', (req, res) => {
-    const str = [{
-        param1: '1',
-        param2: '2',
-        param3: '3',
-        msg: 'This in server default message.',
-    }];
-    res.end(JSON.stringify(str));
-
-    console.log('server got request \'/hey\' ');
-});
 
 
-routesHandler.get('/post1', (req, res) => {
-    res.end('post1');
-});
 
-routesHandler.get('/post', (req, res) => {
-    res.send('Hello from the server. (post)');
-});
+
+
+// ----------- Examples (old - no router) --------------
 
 // //still works
 // app.get('/getparams', (req, res) => {
@@ -53,29 +45,11 @@ routesHandler.get('/post', (req, res) => {
 //         'msg': 'This is server default message.'
 //     }];
 //     res.end(JSON.stringify(str));
-
 //     console.log('server got request \'/getparams\'.');
 //     // res.send('Hello from the server main page');
 // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.use(express.static('public'));
 
 // app.get('/', (req, res) => {
 //     console.log('A new request has arrived to index.js');
 //     res.send('Hello from the server main page');
 // });
-
-
-// TODO: fix/change syntax to work with eslint format.
---
+++ src/routes/default_route.js
@@ -0,0 +1,41 @@
+import express from 'express';
+const router = express.Router(); // router is 'routes handler'
+
+// router.get('/', (req, res) => {
+//     res.send('Hello from the server. ( \'/\' )');
+// });
+
+router.get('/getparams', (req, res) => {
+    const str = [{
+        param1: '1',
+        param2: '2',
+        param3: '3',
+        msg: 'This is server default message.',
+    }];
+    res.end(JSON.stringify(str));
+
+    console.log('server got request \'/getparams\' ');
+});
+
+router.post('/post', (req, res) => {
+    // const param1 = req.body.param1;
+    // res.send('Hello from the server. (/post)');
+    // const default_route = require('default_route');
+    // import default_route from './routes/default_route.js';
+    res.send('Hello from the server. (/post)');
+});
+
+router.post('/post1', (req, res) => {
+    res.end('post1');
+});
+
+router.put('/put1', (req, res) => {
+    res.send('put1');
+});
+
+/** 
+ * Makes default_route usable as separate file. (requires import where used). 
+ * ( 'router' is renamed to 'default_route' in index.js . ) 
+ * */
+// module.exports = router;
+export default router;
