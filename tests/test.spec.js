2d272bb613efb8a74041dc1558a3a9efcf4184e9
KaTziRadar <adarkatzir@gmail.com>
2023-04-05 14:33:56 +0300

    fix tests2

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,7 +1,10 @@
 import { expect } from 'chai';
 
+// eslint-disable-next-line no-undef
 describe('Array', () => {
+    // eslint-disable-next-line no-undef
     describe('#sort', () => {
+        // eslint-disable-next-line no-undef
         it('should sorting array by name', () => {
             const names = ['Anthony', 'Stav', 'Adar', 'Ido', 'Maor', 'Neoray'];
             expect(names.sort()).to.be.eql(['Adar', 'Anthony', 'Ido', 'Maor', 'Neoray', 'Stav']);
