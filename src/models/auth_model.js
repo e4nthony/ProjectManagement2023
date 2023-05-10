7e44fd0322f3f0c62e8b359a99a9833960e13776
Anthony E <e4nthony@gmail.com>
2023-05-10 18:58:49 +0300

    eslint hotfix2

--- src/models/auth_model.js
+++ src/models/auth_model.js
@@ -4,12 +4,12 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
     email: {
         type: String,
-        required: true
+        required: true,
     },
-    enc_password: {         //encrypted password
+    enc_password: {
         type: String,
-        required: true
-    }
+        required: true,
+    },
 });
 
 module.exports = model('User', userSchema);
