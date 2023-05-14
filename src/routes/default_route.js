20edceeb9685b01a6c251900964dc1f505953cb6
Anthony E <e4nthony@gmail.com>
2023-05-15 02:13:03 +0300

    adding debug path to test POST - works

--- src/routes/default_route.js
+++ src/routes/default_route.js
@@ -26,6 +26,13 @@ router.post('/login', (req, res) => {
     res.send({ msg: 'Invalid email or password (server message)' });
 });
 
+router.post('/auth/login', (req, res) => {
+    // let save = req 
+    // to json , to obj
+    // check email & password
+    res.send({ msg: 'Invalid email or password (server message)' });
+});
+
 router.get('/get_all_users_mails', (req, res) => {
 
     const getAllUsers = async () => {
