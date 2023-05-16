46317a0c44218ba52b380c3802b0789096f0ed2b
StavShar <stavshar21@gmail.com>
2023-05-16 04:22:28 +0300

    login fixed (tempUser defined in lines 22-25)

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -16,7 +16,12 @@ const auth_model = require('../models/auth_model')
 
 
 function sendError(res, error_msg) {
-    res.status(400).send({ 'error': error_msg });
+    return res.status(200).send(false);
+}
+
+const tempUser = {
+    email: "asd",
+    password: "qwe"
 }
 
 async function login(req, res) {
@@ -26,39 +31,69 @@ async function login(req, res) {
     error_txt = 'Invalid email or password'
 
     /** Double check that data isn't empty */
-    if (email == null) {
-        return sendError(res, error_txt);   //   Epmty email
+    //    if (email == null) {
+    //        return sendError(res, error_txt);   //   Epmty email
+    //    }
+    //    else if (password == null) {
+    //        return sendError(res, error_txt);   //   Epmty password
+    //    }
+    function check_details() {
+        if (email == tempUser.email && password == tempUser.password)
+            return true;
+        return false;
     }
-    else if (password == null) {
-        return sendError(res, error_txt);   //   Epmty password
-    }
-
-
-    /** try connect to DB,
-     *  check if email in use: 
-     * */
     try {
-        const userAuthData = await auth_model.findOne({ 'email': email });    //  findOne() mongodb's func.
-        if (userAuthData == null) {
-            return sendError(res, error_txt); //  Email not registered yet / not found
+        const userAuthData = await check_details();    //  findOne() mongodb's func.
+        console.log("userAuthData: " + userAuthData);
+        if (userAuthData) {
+            return res.status(200).send(email);
         }
-
-        // TODO: USE PASS ENCRYPTION HERE
-        if (password != userAuthData.enc_password) {
-            return sendError(res, error_txt); //   Wrong password
-        }
-
-        return res.status(200).send({ 'status': 'success' });
+        return res.status(400).send(email);
+        //        if (userAuthData == false) {
+        //            return sendError(res, error_txt); //  Email not registered yet / not found
+        //        }
+        //        else {
+        //            return res.send(true);
+        //        }
     } catch (err) {
         /**
          * expected error:
          * - server lost connection with db
          * */
         console.log('error: ' + err);
-        return sendError(res, 'Unexpected error');
     }
 
     // res.send({ msg: 'default response from server \"auth\\login\" path' });
+
+
+
+
+    //
+    //    /** try connect to DB,
+    //     *  check if email in use: 
+    //     * */
+    //    try {
+    //        const userAuthData = await auth_model.findOne({ 'email': email });    //  findOne() mongodb's func.
+    //        if (userAuthData == null) {
+    //            return sendError(res, error_txt); //  Email not registered yet / not found
+    //        }
+    //
+    //        // TODO: USE PASS ENCRYPTION HERE
+    //        if (password != userAuthData.enc_password) {
+    //            return sendError(res, error_txt); //   Wrong password
+    //        }
+    //
+    //        return res.status(200).send({ 'status': 'success' });
+    //    } catch (err) {
+    //        /**
+    //         * expected error:
+    //         * - server lost connection with db
+    //         * */
+    //        console.log('error: ' + err);
+    //        return sendError(res, 'Unexpected error');
+    //    }
+    //
+    //    // res.send({ msg: 'default response from server \"auth\\login\" path' });
 };
 
 module.exports = { login }
