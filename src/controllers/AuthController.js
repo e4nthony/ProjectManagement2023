2a81fdc6ffb08f1ca058a503bad9ffba4bf1f535
Anthony E <e4nthony@gmail.com>
2023-05-21 04:59:41 +0300

    password encryption works. (BE)

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -11,20 +11,23 @@
 /* Token */
 const jwt = require('jsonwebtoken');
 
+/* Pass Encryption */
+const bcrypt = require('bcryptjs');
+
 /* MongoDB models */
 const AuthModel = require('../models/AuthModel');
 const UserModel = require('../models/UserModel');
 
-
 /* Access Global Variables */
 require('dotenv').config();
+
 
 /**
  * Checks user's registration data
  * and creates new user at database,
  * (creates user in two DB tables, Info and Auth Credentils).
  */
-async function register (req, res) {
+async function register(req, res) {
     try {
         console.log('server got register request: \n' + req.body);  // DEBUG
 
@@ -50,7 +53,7 @@ async function register (req, res) {
     }
 }
 
-function sendLoginError (res, error_msg = 'Invalid email or password') {
+function sendLoginError(res, error_msg = 'Invalid email or password') {
     return res.status(400).send({ error: error_msg });
 }
 
@@ -61,46 +64,60 @@ function sendLoginError (res, error_msg = 'Invalid email or password') {
  *
  * @returns ?response from Frontend?
  */
-async function login (req, res) {
+async function login(req, res) {
     let email = '';
-    let password = '';
+    let enc_password = '';
+
+    if (req.body == null) {
+        console.log('got empty request, sending login error...');
+        return sendLoginError(res);
+    }
+
     try {
         console.log('server got login request: \n' + JSON.stringify(req.body)); // DEBUG
 
         email = req.body.email;
-        password = req.body.password;
+        enc_password = req.body.enc_password;
 
         /* check that data isn't empty */
         if (email == null) {
             return sendLoginError(res);   //   Epmty email
-        } else if (password == null) {
-            return sendLoginError(res);   //   Epmty password
+        } else if (enc_password == null) {
+            return sendLoginError(res);   //   Epmty enc_password
         }
     } catch (err) {
         console.log('login error: ' + err);
         return sendLoginError(res, 'Unexpected error');
     }
 
-
-
     let authData
 
     /* try connect to DB */
     try {
-        console.log('sending \'find user by mail\' request to DB...'); // DEBUG
+        console.log('sending \'find user by mail\' request to DB...');
         authData = await AuthModel.findOne({ email });  //  findOne() mongodb's func.
-        console.log('DB results:\n' + JSON.stringify(authData, null, 2)); // DEBUG
+        console.log('DB results:\n' + JSON.stringify(authData, null, 2));
     } catch (err) {
         /* server might lost connection with DB */
-        // or not in the table?
         console.log('error: ' + err);
         return sendLoginError(res, 'Unexpected error');
     }
 
-    /* check password match to database password */
-    // todo encrypt pass check
-    // todo FIX ERROR WHEN SENDING WRONG PASS and db returns null
-    if (authData.enc_password != password) { return sendLoginError(res); } ; //   Password didn't match
+    /* if user sends unexisting email - DB returns null */
+    if (authData == null) {
+        console.log('User not found in DB, sending login error...');
+        return sendLoginError(res);
+    }
+
+    
+    
+    /* check password's hash match to database password's hash */
+    const is_match = await bcrypt.compare(enc_password, authData.enc_password)
+
+    if (is_match == false) { 
+        console.log('Hash didn\'t match, what means got wrong password, sending login error...');
+        return sendLoginError(res); 
+    }  
 
     /* generate token */
     console.log('generating token...');
@@ -108,10 +125,24 @@ async function login (req, res) {
         { email: 'email' },
         process.env.ACCESS_TOKEN_SECRET,
         { expiresIn: process.env.JWT_TOKEN_EXPIRATION }
-    )
+    );
 
-    console.log('token: ' + accessToken + ', sending to client...');
+    console.log('token: ' + accessToken + ', sending it to client...');
     return res.status(200).json(accessToken);
 };
 
-module.exports = { login, register }
+/**
+ * TODO
+ */
+async function deleteaccount(req, res) {
+    try {
+        console.log('server got delete account request: \n' + req.body);  // DEBUG
+
+        // TODO
+    } catch (err) {
+        console.log('delete account error: ' + err);
+        return res.status(400).send({ error: 'delete account error, please try again later' });
+    }
+}
+
+module.exports = { login, register, deleteaccount }
