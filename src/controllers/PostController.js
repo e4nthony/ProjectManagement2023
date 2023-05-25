9e1ccba63c490f9be678d10ea8083b0b9c07e7e3
Anthony E <e4nthony@gmail.com>
2023-05-25 05:41:00 +0300

    added posts functions and routes - not tested yet current tests pass

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -4,7 +4,7 @@
  * 1XX — Informational
  * 2XX — Success        , 200 - status ok
  * 3XX — Redirection
- * 4XX — Client Error   , 400 - bad request or error
+ * 4XX — Client Error   , 400 - bad request or error, 404 - not found
  * 5XX — Server Error
  */
 
@@ -12,9 +12,9 @@
 const PostModel = require('../models/PostModel');
 
 /* Access Global Variables */
-require('dotenv').config();
+// require('dotenv').config();
 
-function sendDefaultError (res, error_msg = ' Something went wrong.') {
+function sendDefaultError (res, error_msg = 'Something went wrong.') {
     return res.status(400).send({ error: error_msg });
 }
 
@@ -27,8 +27,9 @@ function sendCreatePostError (res, error_msg = 'Create post error, please try ag
  * creates new post at DB,
  * sends response to client. (status 200 if post created successfully)
  *
- * @param {*} req Expected fields in body:  required:  post_tittle, post_text, author_email, starting_price.
+ * Sends fields: msg.
  *
+ * @param {*} req Expected fields in body:  required:  post_tittle, post_text, author_email, starting_price.
  * @param {*} res
  * @returns
  */
@@ -116,10 +117,158 @@ async function create (req, res) {
 //     }
 // }
 
+
+/**
+ * gets post id,
+ * reads existing 'post' at DB by its id,
+ * sends as response to client a single 'post' object. (status 200 - if post found successfully)
+ *                                                     (status 400 - failed)
+ *                                                     (status 404 - if not found)
+ *
+ * Sends fields: a_post.
+ *
+ * @param {*} req Expected fields in body:  required:  post_id.
+ * @param {*} res
+ * @returns
+ */
+async function get_post_by_id (req, res) {
+    try {
+        console.log('server got get_post_by_id post request: \n' + JSON.stringify(req.body, null, 2));
+
+        if (!req.body || !req.body.post_id) {
+            console.log('got corrupted request, sending create post error...');
+            return sendDefaultError(res);
+        }
+
+        console.log('getting post by id from remote DB...');
+        const a_post = await PostModel.find({ _id: req.body.post_id });
+        console.log('post: ' + JSON.stringify(a_post, null, 2));
+
+        if (!a_post) {
+            return res.status(404).send({ error: 'no post found.' });
+        }
+
+        console.log('get_post_by_id is complete, sending post to client...');
+        return res.status(200).send({ a_post });
+    } catch (err) {
+        /* server might lost connection with DB */
+        console.log('create post error: ' + err);
+        return sendCreatePostError(res, 'Unexpected error');
+    }
+}
+
+
+/**
+ * reads at DB,
+ * sends all posts as response to client (obj). (status 200 - if at least one post found)
+ *                                              (status 400 - failed)
+ *                                              (status 404 - if not found posts, db empty)
+ * Sends fields: posts.
+ *
+ * @param {*} req Expected fields in body:  required:  author_email.
+ * @param {*} res
+ * @returns
+ */
+async function get_all_posts_by_author (req, res) {
+    try {
+        console.log('server got get_all_posts_by_author post request: \n' + JSON.stringify(req, null, 2));
+
+        if (!req.body || !req.body.author_email) {
+            console.log('got corrupted request, sending create post error...');
+            return sendDefaultError(res);
+        }
+
+        console.log('getting all posts from remote DB...');
+        const posts = await PostModel.find({ author_email: req.body.author_email });
+        console.log('all posts: ' + JSON.stringify(posts, null, 2));
+
+        if (!posts) {
+            return res.status(404).send({ error: 'no posts found.' });
+        }
+
+        console.log('sending all posts to client...');
+        return res.status(200).send(posts);
+    } catch (err) {
+        /* server might lost connection with DB */
+        console.log('error - get_all_posts: ' + err);
+        return sendDefaultError(res, 'Unexpected error');
+    }
+}
+
+
+/**
+ * reads at DB,
+ * sends all posts as response to client (obj). (status 200 - if at least one post found)
+ *                                              (status 400 - failed)
+ *                                              (status 404 - if not found posts, db empty)
+ * Sends fields: posts.
+ *
+ * @param {*} req Not expected fields.
+ * @param {*} res
+ * @returns
+ */
+async function get_all_posts (req, res) {
+    try {
+        console.log('server got get_all_posts get request: \n' + JSON.stringify(req, null, 2));
+
+        console.log('getting all posts from remote DB...');
+        const posts = await PostModel.find();
+        console.log('all posts: ' + JSON.stringify(posts, null, 2));
+
+        if (!posts) {
+            return res.status(404).send({ error: 'no posts found.' });
+        }
+
+        console.log('sending all posts to client...');
+        return res.status(200).send(posts);
+    } catch (err) {
+        /* server might lost connection with DB */
+        console.log('error - get_all_posts: ' + err);
+        return sendDefaultError(res, 'Unexpected error');
+    }
+}
+
+
+/**
+ * reads 20 newest posts at DB,
+ * sends 20 newest posts as response to client (obj). (status 200 - if at least one post found)
+ *                                                    (status 400 - failed)
+ *                                                    (status 404 - if not found posts, db empty)
+ * Sends fields: posts.
+ *
+ * @param {*} req Not expected fields.
+ * @param {*} res
+ * @returns
+ */
+async function get_20_newest_posts (req, res) {
+    try {
+        console.log('server got get_all_posts get request: \n' + JSON.stringify(req, null, 2));
+
+        console.log('getting get_20_newest_posts from remote DB...');
+        /* finds 20 last records at DB */
+        const posts = await PostModel.find({ $query: {}, $orderby: { $natural: -1 } }).limit(20);
+        console.log('get_20_newest_posts: ' + JSON.stringify(posts, null, 2));
+
+        if (!posts) {
+            return res.status(404).send({ error: 'no posts found.' });
+        }
+
+        console.log('sending get_20_newest_posts to client...');
+        return res.status(200).send(posts);
+    } catch (err) {
+        /* server might lost connection with DB */
+        console.log('error - get_20_newest_posts: ' + err);
+        return sendDefaultError(res, 'Unexpected error');
+    }
+}
+
+
 module.exports = {
-    create
+    create,
     // update_post_by_id,
     // delete_post_by_id,
-    // get_post_by_id,
-    // get_all_posts,
+    get_post_by_id,
+    get_all_posts_by_author,
+    get_all_posts,
+    get_20_newest_posts
 }
--- src/routes/PostRoute.js
+++ src/routes/PostRoute.js
@@ -10,6 +10,26 @@ router.post('/create',                  /* path '/create' use func create from '
     post.create
 );
 
+router.post('/get_post_by_id',          /* path '/get_post_by_id' use func get_post_by_id from 'post' controller file */
+    // AuthMiddleware.validateToken, // with authorization todo enable
+    post.get_post_by_id
+);
+
+router.post('/get_all_posts_by_author', /* path '/get_all_posts_by_author' use func get_all_posts_by_author from 'post' controller file */
+    // AuthMiddleware.validateToken, // with authorization todo enable
+    post.get_all_posts_by_author
+);
+
+router.get('/get_all_posts',            /* path '/get_all_posts' use func get_all_posts from 'post' controller file */
+    // AuthMiddleware.validateToken, // with authorization todo enable
+    post.get_all_posts
+);
+
+router.get('/get_20_newest_posts',      /* path '/get_20_newest_posts' use func get_20_newest_posts from 'post' controller file */
+    // AuthMiddleware.validateToken, // with authorization todo enable
+    post.get_20_newest_posts
+);
+
 // todo enable
 // router.post('/update', post.update);    /* path '/update' use func update from 'post' controller file */
 
