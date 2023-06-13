4bdef0b7176e86ef0fa584247d22ff97fb9b47dd
hazanido <hazanido@gmail.com>
2023-06-13 22:32:36 +0300

    fix chat

--- src/controllers/MessageController.js
+++ src/controllers/MessageController.js
@@ -15,10 +15,15 @@ async function get_message (req, res) {
 async function new_message (req, res) {
     console.log('try to new messages');
     try {
-        const messages = await Message.find({
-            conversationId: req.params.conversationId
+        const new_message = new Message({
+            sender: req.body.sender,
+            text: req.body.text
+                              
         });
-        res.status(200).json(messages);
+        await new_message.save();  
+
+        
+        res.status(200).json(new_message);
     } catch (err) {
         res.status(500).json(err);
     }
--- src/models/Message.js
+++ src/models/Message.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema(
     {
-        conversationId: {
-            type: String
-        },
+        
         sender: {
-            type: String
+            type: String,
+            required: true
         },
         text: {
-            type: String
+            type: String, 
+            required: true
         }
     },
     { timestamps: true }
