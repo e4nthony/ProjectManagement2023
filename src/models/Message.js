16fd80959e72d534cdda6469b31ca95eba4ea5f4
Anthony E <e4nthony@gmail.com>
2023-06-14 01:24:58 +0300

    code style updated

--- src/models/Message.js
+++ src/models/Message.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema(
     {
-        
+
         sender: {
             type: String,
             required: true
         },
         text: {
-            type: String, 
+            type: String,
             required: true
         }
     },
